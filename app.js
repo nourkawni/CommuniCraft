ab12dc6880962d54bc78324f21cb38b93c91da8f
aya-baara <124375259+aya-baara@users.noreply.github.com>
2024-03-21 15:35:03 +0200

    Update app.js

--- app.js
+++ app.js
@@ -5,13 +5,18 @@ const loginRouter = require('./routes/loginRoutes')
 const profileRouter = require('./routes/profileRoutes')
 const borrowRouter = require('./routes/borrowRouter')
 const notificationRouter = require('./routes/notificationRoutes')
-
+const projectLibraryRouter = require('./routes/projectLibraryRoutes1')
+const sellRouter = require('./routes/sellRoutes')
+const chatRouter=require('./routes/external_API_routes')
 
 app.use('/login', loginRouter);
 app.use('/profile', profileRouter);
 app.use('/borrow', borrowRouter);
 app.use('/notification', notificationRouter);
 
+app.use('/projectLibrary', projectLibraryRouter);
+app.use('/sell', sellRouter);
+app.use('/chat',chatRouter)
 
 app.listen(5000, () => {
     console.log("app is listening on the port 5000");
