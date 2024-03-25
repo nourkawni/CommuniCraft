362dff348f4fc4b823fba534abbb56abb8c8e964
noork <nour.mkawni@gmail.com>
2024-03-25 13:44:38 +0200

    Join Team Request

--- app.js
+++ app.js
@@ -1,35 +1,28 @@
 const express = require('express');
 const app = express();
+app.use(express.json());  
 
+// const loginRouter = require('./routes/loginRoutes')
+// const profileRouter = require('./routes/profileRoutes')
+// const borrowRouter = require('./routes/borrowRouter')
+// const notificationRouter = require('./routes/notificationRoutes')
+// const projectLibraryRouter = require('./routes/projectLibraryRoutes1')
+// const sellRouter = require('./routes/sellRoutes')
+// const chatRouter=require('./routes/external_API_routes')
+// const projectRouter=require("./routes/projectLibraryrRoutes")
+const TeamRouter=require("./routes/JoinTeamRoutes")
 
+// app.use('/login', loginRouter);
+// app.use('/api/Project', projectRouter);
 
-
-
-app.use(express.json());
-const loginRouter = require('./routes/loginRoutes')
-const profileRouter = require('./routes/profileRoutes')
-const borrowRouter = require('./routes/borrowRouter')
-const notificationRouter = require('./routes/notificationRoutes')
-const projectLibraryRouter = require('./routes/projectLibraryRoutes1')
-const sellRouter = require('./routes/sellRoutes')
-const chatRouter=require('./routes/external_API_routes')
-
-const projectRouter=require("./routes/projectLibraryrRoutes")
-
-
-app.use('/login', loginRouter);
-app.use('/api/Project', projectRouter);
-
-app.use('/login', loginRouter);
-app.use('/profile', profileRouter);
-app.use('/borrow', borrowRouter);
-app.use('/notification', notificationRouter);
-
-app.use('/projectLibrary', projectLibraryRouter);
-app.use('/sell', sellRouter);
-app.use('/chat',chatRouter)
-
-app.listen(2000, () => {
-    console.log("app is listening on the port 2000");
+// app.use('/login', loginRouter);
+// app.use('/profile', profileRouter);
+// app.use('/borrow', borrowRouter);
+// app.use('/notification', notificationRouter);
+// app.use('/projectLibrary', projectLibraryRouter);
+// app.use('/sell', sellRouter);
+//app.use('/chat',chatRouter);
+app.use('/joinTeam', TeamRouter);
+app.listen(5000, () => {
+    console.log("app is listening on the port 5000");
 });
-
